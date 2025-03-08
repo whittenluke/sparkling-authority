f3a5a285ea54db91cd1ed4725ca89ac71714bb03
Luke Whitten <whittenluke@gmail.com>
2025-03-08 18:52:48 -0500

    Removed CookieOptions type

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -2,17 +2,6 @@ import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { Database } from './database.types'
 
-type CookieOptions = {
-  name: string
-  value: string
-  maxAge?: number
-  httpOnly?: boolean
-  secure?: boolean
-  path?: string
-  domain?: string
-  sameSite?: 'strict' | 'lax' | 'none'
-}
-
 export function createClient() {
   return createServerComponentClient<Database>({
     cookies
