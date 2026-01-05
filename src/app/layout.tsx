ef0f2fa6105e873aff685fb0e5f948af52d5d12d
Luke Whitten <whittenluke@gmail.com>
2026-01-05 12:45:38 -0500

    Add Google Tag Manager integration to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,30 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning className="light">
       <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WL334G4V');`,
+          }}
+        />
+        {/* End Google Tag Manager */}
         <link href="https://api.fontshare.com/v2/css?f[]=clash-display@400,500,600,700&display=swap" rel="stylesheet" />
       </head>
       <body className={`${inter.className} ${plusJakartaSans.variable} app-background`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WL334G4V"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <ThemeProvider defaultTheme="system" storageKey="sparkling-authority-theme">
           <AuthProvider>
             {children}
