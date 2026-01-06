d67ea995414f0307addab70e47c74f1ad8dfd354
Luke Whitten <whittenluke@gmail.com>
2026-01-05 20:54:30 -0500

    Update footer privacy link text to Privacy Policy

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -53,7 +53,7 @@ export function Footer() {
                 <ul role="list" className="mt-4 space-y-4">
                   <li>
                     <Link href="/privacy" className="text-sm text-muted-foreground hover:text-foreground">
-                      Privacy
+                      Privacy Policy
                     </Link>
                   </li>
                   <li>
